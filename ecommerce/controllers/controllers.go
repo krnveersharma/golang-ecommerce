8de6683d2c2785c5c9026182d3c4d909cbe01051
krnveersharma <115925693+krnveersharma@users.noreply.github.com>
2024-08-28 16:23:07 +0530

    Done with database setup

--- ecommerce/controllers/controllers.go
+++ ecommerce/controllers/controllers.go
@@ -1,5 +1,31 @@
 package controllers
 
-func controllers() {
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, givenPassword string) bool {
+
+}
+
+func Signup() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProduct() gin.HandlerFunc {
+
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
 
 }
